98fca1de3f68d9940b672d07075e151a3ec3d28e
rhannska <rhannska@users.noreply.github.com>
2024-02-28 14:27:11 -0600

    added delete

--- web.py
+++ web.py
@@ -8,16 +8,18 @@ def add_todo():
     todos.append(todo)
     functions.write_todos(todos)
 
-todos = functions.get_todos()
 
 st.title("Checklist App")
 st.subheader("This is my first python web app")
 st.write("Just plowing right through all these videos here")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label="Add something:", placeholder="Add something here...",
               on_change=add_todo, key='input_todo')
-
-st.session_state
