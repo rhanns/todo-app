06a67836d09bf34913f82d060ed531607ea9f3c6
rhannska <rhannska@users.noreply.github.com>
2024-02-28 14:17:02 -0600

    implement add new todo with input bar

--- web.py
+++ web.py
@@ -1,6 +1,13 @@
 import streamlit as st
 import functions
 
+
+todos = functions.get_todos()
+def add_todo():
+    todo = st.session_state["input_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
 todos = functions.get_todos()
 
 st.title("Checklist App")
@@ -10,4 +17,7 @@ st.write("Just plowing right through all these videos here")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="Add something:", placeholder=("Add something here..."))
+st.text_input(label="Add something:", placeholder="Add something here...",
+              on_change=add_todo, key='input_todo')
+
+st.session_state
