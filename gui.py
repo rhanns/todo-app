53fe8d8dbc21d2fe3791f00e1416f94471d0193a
rhannska <rhannska@users.noreply.github.com>
2024-02-28 11:42:21 -0600

    Implement Exit button

--- gui.py
+++ gui.py
@@ -7,9 +7,14 @@ add_button = gui.Button("Add")
 list_box = gui.Listbox(values=functions.get_todos(), key='todos',
                        enable_events=True, size=[45, 10])
 edit_button = gui.Button("Edit")
+complete_button = gui.Button("Complete")
+exit_button = gui.Button("Exit")
 
 window = gui.Window('My To-Do App',
-                    layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                    layout=[[label],
+                            [input_box, add_button],
+                            [list_box, edit_button, complete_button],
+                            [exit_button]],
                     font=('Helvetica', 20))
 while True:
     event, values = window.read()
@@ -32,8 +37,17 @@ while True:
             todos[index] = new_todo
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+        case "Complete":
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
         case 'todos':
             window['todo'].update(values['todos'][0])
+        case 'Exit':
+            break
         case gui.WIN_CLOSED:
             break
 
